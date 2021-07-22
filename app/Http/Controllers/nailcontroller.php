a190a1f870cd4876bba6cf0864e3e334471438b6
nishamr65 <mca537@rajagiri.edu>
2021-07-21 22:25:55 -1200

    djhebd

--- app/Http/Controllers/nailcontroller.php
+++ app/Http/Controllers/nailcontroller.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\nailmodel;
+use App\Models\bookingmodels;
 use Illuminate\Support\Facades\DB;
 class nailcontroller extends Controller
 {
